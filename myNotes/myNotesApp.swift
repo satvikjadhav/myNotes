7a9551923b9b92aac8a71f4e1bbb56df1bff7d59
satvikjadhav <satvikj5@gmail.com>
2025-02-07 15:17:19 -0600

    Initial Commit

---
+++ myNotes/myNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  myNotesApp.swift
+//  myNotes
+//
+//  Created by Satvik  Jadhav on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct myNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
